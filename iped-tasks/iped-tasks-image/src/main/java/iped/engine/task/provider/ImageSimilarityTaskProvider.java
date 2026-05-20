40bd58dae1938c1228559e4cf32fac852b7c97cb
bhoelz <bhoelz@hotmail.com>
2026-05-19 21:35:48 -0300

    refactor(tasks): migrate image similarity task to iped-tasks-image via SPI

---
+++ iped-tasks/iped-tasks-image/src/main/java/iped/engine/task/provider/ImageSimilarityTaskProvider.java
@@ -0,0 +1,29 @@
+package iped.engine.task.provider;
+
+import java.util.List;
+
+import iped.engine.task.AbstractTask;
+import iped.engine.task.similarity.ImageSimilarityTask;
+import iped.tasks.spi.TaskDependency;
+import iped.tasks.spi.TaskDescriptor;
+import iped.tasks.spi.TaskProvider;
+
+public class ImageSimilarityTaskProvider implements TaskProvider<AbstractTask> {
+
+    private static final String TASK_ID = "iped.engine.task.similarity.ImageSimilarityTask";
+
+    @Override
+    public TaskDescriptor descriptor() {
+        return TaskDescriptor.of(TASK_ID, dependencies());
+    }
+
+    @Override
+    public AbstractTask createTask() {
+        return new ImageSimilarityTask();
+    }
+
+    @Override
+    public List<TaskDependency> dependencies() {
+        return List.of(TaskDependency.after("iped.engine.task.ImageThumbTask"));
+    }
+}
