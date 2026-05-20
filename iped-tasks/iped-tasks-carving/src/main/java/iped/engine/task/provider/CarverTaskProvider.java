da7373550845c60dc2fd8380d018d5043792ca71
bhoelz <bhoelz@hotmail.com>
2026-05-19 21:47:09 -0300

    refactor(tasks): migrate carving tasks to iped-tasks-carving via SPI

---
+++ iped-tasks/iped-tasks-carving/src/main/java/iped/engine/task/provider/CarverTaskProvider.java
@@ -0,0 +1,29 @@
+package iped.engine.task.provider;
+
+import java.util.List;
+
+import iped.engine.task.AbstractTask;
+import iped.engine.task.carver.CarverTask;
+import iped.tasks.spi.TaskDependency;
+import iped.tasks.spi.TaskDescriptor;
+import iped.tasks.spi.TaskProvider;
+
+public class CarverTaskProvider implements TaskProvider<AbstractTask> {
+
+    private static final String TASK_ID = "iped.engine.task.carver.CarverTask";
+
+    @Override
+    public TaskDescriptor descriptor() {
+        return TaskDescriptor.of(TASK_ID, dependencies());
+    }
+
+    @Override
+    public AbstractTask createTask() {
+        return new CarverTask();
+    }
+
+    @Override
+    public List<TaskDependency> dependencies() {
+        return List.of(TaskDependency.after("iped.engine.task.carver.LedCarveTask"));
+    }
+}
---
+++ iped-tasks/iped-tasks-carving/src/main/java/iped/engine/task/provider/KnownMetCarveTaskProvider.java
@@ -0,0 +1,29 @@
+package iped.engine.task.provider;
+
+import java.util.List;
+
+import iped.engine.task.AbstractTask;
+import iped.engine.task.carver.KnownMetCarveTask;
+import iped.tasks.spi.TaskDependency;
+import iped.tasks.spi.TaskDescriptor;
+import iped.tasks.spi.TaskProvider;
+
+public class KnownMetCarveTaskProvider implements TaskProvider<AbstractTask> {
+
+    private static final String TASK_ID = "iped.engine.task.carver.KnownMetCarveTask";
+
+    @Override
+    public TaskDescriptor descriptor() {
+        return TaskDescriptor.of(TASK_ID, dependencies());
+    }
+
+    @Override
+    public AbstractTask createTask() {
+        return new KnownMetCarveTask();
+    }
+
+    @Override
+    public List<TaskDependency> dependencies() {
+        return List.of(TaskDependency.after("iped.engine.task.carver.CarverTask"));
+    }
+}
